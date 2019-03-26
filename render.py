5b4e5d4b9eae24a0edcf8889a9a9fda08f8c4b58
Mäcks MacBook Pro <maeck@host083.lab.ins.hsr.ch>
2019-03-26 11:28:00 +0100

    Version V1.1

--- render.py
+++ render.py
@@ -5,7 +5,7 @@ import json
 from time import sleep
 
 
-
+#Script wird alle 60s ausgeführt
 while True:
 
     # Load Jinja2 template
@@ -16,13 +16,13 @@ while True:
     yaml_file = open('config.yml')
     config = yaml.load(yaml_file)
 
-    # Render configs and print
+    # Render config and print
     for device in config:
 
         rendered_config = template.render(**device)
         print(rendered_config)
 
-
+    #Connect to switch via napalm
     driver = napalm.get_network_driver('ios')
 
     device_information = {'hostname': '10.3.255.101',
@@ -31,12 +31,13 @@ while True:
 
     print('Accessing 10.3.255.101 ...')
 
-    # device.load_merge_candidat  - Napalm
+    #Config auf Switch mit Template vergleichen
     with driver(**device_information) as device:
         device.load_replace_candidate(config=rendered_config)
         print('\nDiff:')
         print(device.compare_config())
 
+#Wenn Config geändert hat, mit Template überschreiben
         diffs = device.compare_config()
         if len(diffs) > 0:
             print('Changes need to be applied ...')
@@ -44,4 +45,6 @@ while True:
         else:
             print('No Changes needed ...')
             device.discard_config()
-sleep(60)
+
+        sleep(60)
+        
